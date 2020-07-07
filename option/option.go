a5a6cf050e393dd6642eab6b9640fc21676426a3
jacexh <jace.xh@gmail.com>
2020-07-07 16:57:06 +0800

    修改配置项

--- option/option.go
+++ option/option.go
@@ -12,12 +12,12 @@ type (
 		Username     string
 		Password     string
 		Name         string `default:"deepmock"`
-		ConnectRetry int    `default:"3"` // 解决istio启动的问题
+		ConnectRetry int    `default:"3" yaml:"connect_retry" json:"connect_retry"` // 解决istio启动的问题
 	}
 
 	ServerOption struct {
 		Port     string `default:":16600"`
-		KeyFile  string
-		CertFile string
+		KeyFile  string `yaml:"key_file,omitempty" json:"key_file,omitempty"`
+		CertFile string `yaml:"cert_file,omitempty" json:"cert_file,omitempty"`
 	}
 )
